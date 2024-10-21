e6bb0ca6ec267a501e600b8f8585d759b6b3825d
bhavinparmar005 <bhavinparmar005@gmail.com>
2024-10-21 00:49:33 -0700

    he

--- Assat/js/script.js
+++ Assat/js/script.js
@@ -1,4 +1,28 @@
-function hello(){
+//target to form
+let form = document.getElementById("registration_form");
 
-    alert(" Java script is on work");
-}
+//target first name Error
+let first_name_error = document.getElementById("error_massage_form_first_name");
+
+//target to email Error
+let email_error = document.getElementById("error_massage_form_email");
+
+//target to mobile number Error
+let mobile_number_error = document.getElementById(
+  "error_massage_form_mobile_number"
+);
+
+//target to password Error
+let password_error = document.getElementById("error_massage_form_password");
+
+//target to conform password Error
+let conform_password_error = document.getElementById(
+  "error_massage_form_conform_password"
+);
+
+form.addEventListener('submit',(e)=>{
+
+e.preventDefault(); 
+
+    console.log("hello");
+});
